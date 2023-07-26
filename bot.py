398efd2eeab49baa5b6684767fc40657103fd66c
Matrix TG <108185896+TGxMATRIX@users.noreply.github.com>
2023-07-26 15:33:15 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -34,11 +34,18 @@ gif = [
 async def approve(_, m : Message):
     op = m.chat
     kk = m.from_user
+    buttons = [[
+                 InlineKeyboardButton("🗯 Channel", url="https://t.me/SDBOTs_inifinity"),
+                 InlineKeyboardButton("💬 Support", url="https://t.me/SDBOTz")
+                 ],[
+                 InlineKeyboardButton("➕ Add me to your Chat ➕", url="https://t.me/SDAutoApproveBot?startgroup")
+              ]]                      
+    reply_markup = InlineKeyboardMarkup(buttons)
     try:
         add_group(m.chat.id)
         await app.approve_chat_join_request(op.id, kk.id)
         img = random.choice(gif)
-        await app.send_video(kk.id,img, "**Hello {}!\nWelcome To {}\n\n__Powerd By : @SdBotz__**".format(m.from_user.mention, m.chat.title))
+        await app.send_message(kk.id, "**Hello {}!\nWelcome To {}\n\n__Powerd By : @SdBotz__**".format(m.from_user.mention, m.chat.title), reply_markup=reply_markup)
         add_user(kk.id)
     except errors.PeerIdInvalid as e:
         print("user isn't start bot(means group)")
